5dd413b97c1a78f2a4c8e5d878c20937ff6a7e9b
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-15 16:59:23 +0100

    Eercizio finito

--- results.php
+++ results.php
@@ -41,7 +41,7 @@ $paragrafoCensurato = str_ireplace($userWord, '***', $userText);
 
     <div id="censured-paragraph" class="d-flex flex-column pe-3">
             <?php
-            // Stampa il paragrafo censurato e la sua lunghezza
+            // Stampo il paragrafo censurato e la sua lunghezza
             echo "<h2>Paragrafo censurato:</h2>";
             echo "<p>$paragrafoCensurato</p>";
             echo "<p>Lunghezza del paragrafo censurato: " . strlen($paragrafoCensurato) . "</p>";
